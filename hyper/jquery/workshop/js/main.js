8d11d0acc826a3e21bdff6127b841e799f008e72
pushmatrix <daniel.beauchamp@gmail.com>
2013-10-03 02:42:24 +0200

    jquery WIP

--- hyper/jquery/workshop/js/main.js
+++ hyper/jquery/workshop/js/main.js
@@ -1,30 +1,13 @@
-$(document).ready(function() {
+$(function() {
 
-  //////////////
-  // EXERCISE 2
-  // Add your code below here so that an alert message pops up when you press that button
 
-  //////////////
-  // EXERCISE 3
-
-  /// "Fade out" spell
-
-  /// "Fade in" spell
-
-  // "Transformation" spell
-
-  // "Hover" spell
-
-  //////////////
-  // EXERCISE 4
-
-  //////////////
-  // EXERCISE 5
-  $('#add-to-cart').click(addToCart);
-  var numItems = 0;
-  function addToCart() {
-    console.log("added an item!");
-  }
+  $('#exercise2Button').on('click', 
+    function() 
+    {
+      $(".dolls").fadeOut();
+      alert("say something cool!!!!");
+    }
+  );
 
 
 
---
+++ hyper/jquery2/workshop/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  /////////
+  // EXERCISE 2
+
+
+  /////////
+  // EXERCISE 3
+
+  // Here is a JavaScript object representing the King of Sweden
+  var person = {
+    name: "Carl Gustaf",
+    occupation: "King",
+    gender: "Male",
+    photo: "http://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Royal_Wedding_Stockholm_2010-Konserthuset-433.jpg/440px-Royal_Wedding_Stockholm_2010-Konserthuset-433.jpg",
+    age: 67
+  };
+
+  // Using this object, fill out the contact form that you see (so it looks like the screenshot)
+  // You can only use JavaScript! 
+  // You are not allowed to modify the index.html file manually!
+
+
+  /////////
+  // EXERCISE 4
+
+});
